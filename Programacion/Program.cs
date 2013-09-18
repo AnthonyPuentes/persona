73de2c520a902113c9a23f2aef762778d69a8cdb
Anthony <Anthony189213@gmail.com>
2013-09-18 11:42:10 -0500

    practica 1

---
+++ Programacion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programa
+{
+    class persona
+    {
+
+        public string nombre;
+        public string correo;
+        public int edad;
+
+        public static void Main(string[] args)
+        {
+            persona p = new persona();
+
+            Console.WriteLine("Cual es tu nombre");
+            p.nombre = Console.ReadLine();
+
+            Console.WriteLine("Cual es tu correo!");
+            p.correo = Console.ReadLine();
+            Console.WriteLine("Cual es tu edad!");
+            p.edad = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Mi nombre es:\n " + p.nombre + "\nMi correo es:\nd " + p.correo + "\nTengo " + p.edad + " años");
+
+            Console.Read();
+
+
+
+
+        }
+    }
+}
